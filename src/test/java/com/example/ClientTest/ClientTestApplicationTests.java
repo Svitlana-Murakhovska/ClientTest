dbaa0a65674d5eb18e21cb4c2abb62dfbc4817ba
Svitlana Murakhovska <svitlana_murakhovska@epam.com>
2023-05-24 11:07:09 +0300

    Initial commit

---
+++ src/test/java/com/example/ClientTest/ClientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ClientTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
